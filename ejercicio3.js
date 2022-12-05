969dd6d8170984aa620a43272f2c0bf13cf706f7
DamarisCaballero <damariscallero011@gmail.com>
2022-12-04 20:43:41 -0600

    mis ejercicios
    3,5,6

---
+++ ejercicio3.js
@@ -0,0 +1,5 @@
+//Determinar la nota final de un alumno, la cual depende de lo siguiente: Examen =20% , tareas 40%,
+//asistencia =10% e investigación =30% , al final deberá mostrar los datos del alumno , nombre , carnet
+//y nota final. Para este ejercicio deberá de utilizar una fuction y asignar parámetros para llenar los
+//valores.
+
